43b6ae15d7f4a852a87175e789af93b1cd24050a
Rennan <rennan24@gmail.com>
2019-11-09 15:10:53 -0800

    Added DashBehaviour and DashTrailManager

---
+++ Assets/Scripts/DashBehaviour.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+
+public class DashBehaviour : MonoBehaviour
+{
+    [Header("Movement Vars:")]
+    public float Speed;
+    public float Length;
+    public float Acceleration;
+
+    [Header("Trail Vars:")]
+    public float SpawnTime;
+    public float FadeTime;
+
+    [SerializeField]
+    private Rigidbody2D body2D;
+
+    [SerializeField]
+    private SpriteRenderer spriteRef;
+
+    private Vector2 curVel;
+    private Vector2 targetVel;
+    private float dashTimer;
+    private float trailTimer;
+
+    public bool IsDashing => dashTimer > 0f;
+
+    private void Update()
+    {
+        if (dashTimer <= 0f)
+            return;
+
+        curVel = Vector2.MoveTowards(curVel, targetVel, Acceleration * Time.deltaTime);
+        dashTimer -= Time.deltaTime;
+        trailTimer += Time.deltaTime;
+
+        if (trailTimer >= SpawnTime)
+        {
+            DashTrailManager.SpawnTrail(spriteRef, FadeTime, transform);
+            trailTimer = 0;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (dashTimer <= 0f)
+            return;
+
+        body2D.velocity = curVel;
+    }
+
+    public void Dash(Vector2 dir)
+    {
+        targetVel = dir * Speed;
+        curVel = body2D.velocity;
+        dashTimer = Length;
+        trailTimer = 0;
+    }
+}
---
+++ Assets/Scripts/DashTrailManager.cs
@@ -0,0 +1,17 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class DashTrailManager : MonoBehaviourSingleton<DashTrailManager>
+{
+    public SpriteRenderer DashSpritePrefab;
+
+    public static void SpawnTrail(SpriteRenderer sprite, float fadeTime, Transform trans)
+    {
+        var trail = Instantiate(Inst.DashSpritePrefab, trans.position, trans.rotation);
+        trail.sprite = sprite.sprite;
+        trail.transform.localScale = trans.localScale;
+
+        trail.DOFade(0f, fadeTime);
+        Destroy(trail.gameObject, fadeTime);
+    }
+}
